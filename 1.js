52f7233e45f1f2c043b44c3ecf2eff56d36613c4
AntonFartovii <anton@rza.by>
2023-06-09 10:25:03 +0300

    feat: 05 task

--- 1.js
+++ 1.js
@@ -1,32 +1,19 @@
-const array = [
-  { country: 'Belarus', city: 'Brest' },
-{ country: 'Russia', city: 'Omsk' },
-{ country: 'Russia', city: 'Samara' },
-{ country: 'Belarus', city: 'Grodno' },
-{ country: 'Belarus', city: 'Minsk' },
-{ country: 'Poland', city: 'Lodz' }
-];
-
-const keySelector = item => item.country;
-const valueSelector = item => item.city;
-// *            =>
-// *   Map {
-// *    "Belarus" => ["Brest", "Grodno", "Minsk"],
-// *    "Russia" => ["Omsk", "Samara"],
-// *    "Poland" => ["Lodz"]
-//   *   }
+// * @example :
+// *    Date(1900,1,1)    => false
+// *    Date(2000,1,1)    => true
+// *    Date(2001,1,1)    => false
+// *    Date(2012,1,1)    => true
+// *    Date(2015,1,1)    => false
 // */
-function group(array, keySelector, valueSelector) {
-  const map = new Map();
-  array.forEach((item) => {
-    const values = map.get(keySelector(item));
-    if (values) {
-      map.set(keySelector(item), [...values, valueSelector(item)]);
-    } else {
-      map.set(keySelector(item), [valueSelector(item)]);
-    }
-  });
-  return map;
+function isLeapYear(date) {
+
 }
 
-group(array, keySelector, valueSelector)
+console.log(new Date(1900, 1, 1));;
+
+console.log(new Date(2000, 1, 1));;
+console.log(new Date(2001, 1, 1));;
+
+console.log(new Date(2012, 1, 1));;
+const date = new Date(2015,1,1);
+console.log(date.getFullYear());
